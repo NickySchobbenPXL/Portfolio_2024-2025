91a99a69246ca631cba3ed95edf1194e95ebdd9d
nicky.schobben@student.pxl.be <nicky.schobben@student.pxl.be>
2025-04-12 15:14:34 +0200

    finalized changes

--- src/components/Navbar/NavbarComponent.vue
+++ src/components/Navbar/NavbarComponent.vue
@@ -1,15 +1,16 @@
 <template>
 
-  <nav class="bg-white border-slate border-[1px]">
+  <nav class="bg-blue-700 border-slate border-[1px]">
 
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
         <img :src="logoPxl" class="h-8" alt="PxlLogo"/>
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">I-talent 2024-2025</span>
+        <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">I-talent 2024-2025</span>
       </a>
 
+      <!-- Start hamburger menu -->
       <button data-collapse-toggle="navbar-dropdown" type="button"
-              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200"
               aria-controls="navbar-dropdown" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -17,27 +18,27 @@
                 d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
       </button>
+      <!-- End hamburger menu -->
 
       <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
 
-        <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-blue-700">
 
           <router-link to="/">
-            <li>
-              <a class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
-                 aria-current="page">Home</a>
+            <li class='group max-lg:border-b max-lg:px-3 max-lg:py-3 relative bg-blue-700'>
+              <a class='hover:text-[#007bff] hover:fill-[#007bff] text-white font-semibold text-[15px] block'>Home</a>
             </li>
           </router-link>
 
           <router-link to="/activiteiten">
-            <li class='group max-lg:border-b max-lg:px-3 max-lg:py-3 relative'>
-              <a class='hover:text-[#007bff] hover:fill-[#007bff] text-gray-600 font-semibold text-[15px] block'>Activiteiten
+            <li class='group max-lg:border-b max-lg:px-3 max-lg:py-3 relative bg-blue-700'>
+              <a class='hover:text-[#007bff] hover:fill-[#007bff] text-white font-semibold text-[15px] block'>Activiteiten
               </a>
             </li>
           </router-link>
 
-          <li class='group max-lg:border-b max-lg:px-3 max-lg:py-3 relative'>
-            <a class='hover:text-[#007bff] hover:fill-[#007bff] text-gray-600 font-semibold text-[15px] block'>Geselecteerde
+          <li class='group max-lg:border-b max-lg:px-3 max-lg:py-3 relative bg-blue-700'>
+            <a class='hover:text-[#007bff] hover:fill-[#007bff] font-semibold text-[15px] block text-white'>Geselecteerde
               activiteiten
               <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" class="ml-1 inline-block"
                    viewBox="0 0 24 24">
@@ -84,8 +85,8 @@
           </li>
 
           <router-link to="/test">
-            <li>
-              <a class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Reflectie</a>
+            <li class='group max-lg:border-b max-lg:px-3 max-lg:py-3 relative bg-blue-700'>
+              <a class='hover:text-[#007bff] hover:fill-[#007bff] text-white font-semibold text-[15px] block'>Reflectie</a>
             </li>
           </router-link>
 
